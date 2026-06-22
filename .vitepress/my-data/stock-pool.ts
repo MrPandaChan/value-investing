ebfd60df4cd40cddd87c3d10d4642da9aeeeae17
chenguobao <chenguobao@ava.com.cn>
2026-06-22 10:17:59 +0800

    docs: update plan

--- .vitepress/my-data/stock-pool.ts
+++ .vitepress/my-data/stock-pool.ts
@@ -70,7 +70,6 @@ const stocks: StockItem[] = [
     // 腾讯控股
     code: "00700",
     industry: Industry.INTERNET,
-    sharesHeld: 300,
     dividendAdjust: 0.8,
     url: "/value-investing/industry/互联网/腾讯控股/",
     plan: {
@@ -87,14 +86,14 @@ const stocks: StockItem[] = [
     // 福耀玻璃
     code: "600660",
     industry: Industry.AUTOMOTIVE_AND_PARTS,
-    sharesHeld: 1200,
+    sharesHeld: 400,
     url: "/value-investing/industry/汽车/福耀玻璃/",
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.1,
       price: [
-        { value: 47.5, quantity: 400 },
-        { value: 45, quantity: 400 },
+        { value: 48.5, quantity: 100 },
+        { value: 46, quantity: 100 },
       ],
     },
   },
@@ -102,33 +101,32 @@ const stocks: StockItem[] = [
     // 云南白药
     code: "000538",
     industry: Industry.TRADITIONAL_CHINESE_MEDICINE,
-    sharesHeld: 600,
+    sharesHeld: 200,
     url: "/value-investing/industry/中药/云南白药/",
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.05,
-      price: [{ value: 46.5, quantity: 400 }],
+      price: [{ value: 46.5, quantity: 100 }],
     },
   },
   {
     // 东阿阿胶
     code: "000423",
     industry: Industry.TRADITIONAL_CHINESE_MEDICINE,
-    sharesHeld: 500,
+    sharesHeld: 200,
     dividendPerYear: 2,
     remark: "几乎100%分红率，一般6月/9月分红",
     url: "/value-investing/industry/中药/东阿阿胶/",
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.05,
-      price: [{ value: 44, quantity: 400 }],
+      price: [{ value: 44, quantity: 100 }],
     },
   },
   {
     // 羚锐制药
     code: "600285",
     industry: Industry.TRADITIONAL_CHINESE_MEDICINE,
-    sharesHeld: 300,
     dividendPerYear: 1,
     remark: "预估6月中分红",
     url: "/value-investing/industry/中药/羚锐制药/",
@@ -136,8 +134,8 @@ const stocks: StockItem[] = [
       type: PlanType.PRICE,
       maxPositionRatio: 0.03,
       price: [
-        { value: 19.1, quantity: 400 },
-        { value: 18.1, quantity: 500 },
+        { value: 19.1, quantity: 200 },
+        { value: 18.1, quantity: 300 },
       ],
     },
   },
@@ -145,14 +143,14 @@ const stocks: StockItem[] = [
     // 分众传媒
     code: "002027",
     industry: Industry.MEDIA,
-    sharesHeld: 1600,
+    sharesHeld: 800,
     url: "/value-investing/industry/传媒/分众传媒/",
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.05,
       dividend: [
-        { value: 0.07, quantity: 2200 },
-        { value: 0.075, quantity: 3000 },
+        { value: 0.07, quantity: 900 },
+        { value: 0.075, quantity: 1000 },
       ],
     },
   },
@@ -173,28 +171,28 @@ const stocks: StockItem[] = [
     // 青岛港A
     code: "601298",
     industry: Industry.SEAPORTS_AND_SERVICES,
-    sharesHeld: 4000,
+    sharesHeld: 1500,
     url: "/value-investing/industry/港口/青岛港/",
     dividendPerYear: 2,
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.1,
-      dividend: [{ value: 0.047, quantity: 2000 }],
+      dividend: [{ value: 0.047, quantity: 1000 }],
     },
   },
   {
     // 永新股份
     code: "002014",
     industry: Industry.PAPER_AND_PACKAGING,
-    sharesHeld: 2000,
+    sharesHeld: 800,
     url: "/value-investing/industry/塑料包装/永新股份/",
     remark: "2025年分红率有降低，分红比过去两年低",
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.05,
       price: [
-        { value: 9.7, quantity: 1400 },
-        { value: 9.2, quantity: 1600 },
+        { value: 9.7, quantity: 500 },
+        { value: 9.2, quantity: 500 },
       ],
     },
   },
@@ -202,16 +200,13 @@ const stocks: StockItem[] = [
     // 招商银行
     code: "600036",
     industry: Industry.BANKING,
-    sharesHeld: 1000,
+    sharesHeld: 500,
     dividendPerYear: 2,
     remark: "预估07-15左右分红，分完后计划减1块",
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.1,
-      price: [
-        { value: 36.5, quantity: 600 },
-        { value: 35.5, quantity: 800 },
-      ],
+      price: [{ value: 36.5, quantity: 300 }],
     },
   },
   {
@@ -226,8 +221,8 @@ const stocks: StockItem[] = [
       type: PlanType.PRICE,
       maxPositionRatio: 0.1,
       price: [
-        { value: 75, quantity: 400 },
-        { value: 71, quantity: 600 },
+        { value: 75, quantity: 100 },
+        { value: 71, quantity: 200 },
       ],
     },
   },
@@ -235,21 +230,21 @@ const stocks: StockItem[] = [
     // 海尔智家
     code: "600690",
     industry: Industry.HOME_APPLIANCES,
-    sharesHeld: 3600,
+    sharesHeld: 1500,
     url: "/value-investing/industry/家电/海尔/",
     dividendPerYear: 2,
     remark: "预估7月/8月分红",
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.08,
-      price: [{ value: 18.5, quantity: 1000 }],
+      price: [{ value: 18.5, quantity: 300 }],
     },
   },
   {
     // 格力电器
     code: "000651",
     industry: Industry.HOME_APPLIANCES,
-    sharesHeld: 1200,
+    sharesHeld: 400,
     url: "/value-investing/industry/家电/格力/",
     dividendPerYear: 2,
     remark: "预估8月分红",
@@ -263,16 +258,13 @@ const stocks: StockItem[] = [
     // 中国移动
     code: "600941",
     industry: Industry.TELECOMMUNICATION_SERVICES,
-    sharesHeld: 500,
+    sharesHeld: 400,
     url: "/value-investing/industry/电信服务/中国移动/",
     dividendPerYear: 2,
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.1,
-      price: [
-        { value: 91, quantity: 100 },
-        { value: 88, quantity: 200 },
-      ],
+      price: [{ value: 88, quantity: 100 }],
     },
   },
   {
@@ -292,7 +284,6 @@ const stocks: StockItem[] = [
     // 中国铁塔
     code: "00788",
     industry: Industry.TELECOMMUNICATION_SERVICES,
-    sharesHeld: 2000,
     url: "/value-investing/industry/电信服务/中国铁塔/",
     dividendPerYear: 2,
     dividendAdjust: 0.8 * 1.3,
@@ -307,28 +298,28 @@ const stocks: StockItem[] = [
     // 长江电力
     code: "600900",
     industry: Industry.ELECTRIC_POWER,
-    sharesHeld: 1200,
+    sharesHeld: 500,
     url: "/value-investing/industry/电力/长江电力/",
     dividendPerYear: 2,
     remark: "预估7月中分红",
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.1,
-      dividend: [{ value: 0.04, quantity: 2000 }],
+      dividend: [{ value: 0.04, quantity: 500 }],
     },
   },
   {
     // 国投电力
     code: "600886",
     industry: Industry.ELECTRIC_POWER,
-    sharesHeld: 1200,
+    sharesHeld: 900,
     url: "/value-investing/industry/电力/国投电力/",
     dividendPerYear: 1,
     remark: "预估8月分红",
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.08,
-      dividend: [{ value: 0.04, quantity: 3000 }],
+      dividend: [{ value: 0.04, quantity: 600 }],
     },
   },
   {
@@ -341,7 +332,7 @@ const stocks: StockItem[] = [
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.2,
-      dividend: [{ value: 0.055, quantity: 1400 }],
+      dividend: [{ value: 0.055, quantity: 500 }],
     },
   },
   {
@@ -361,16 +352,16 @@ const stocks: StockItem[] = [
     // 紫金矿业
     code: "601899",
     industry: Industry.NON_FERROUS_METALS,
-    sharesHeld: 300,
+    sharesHeld: 500,
     url: "/value-investing/industry/有色金属/紫金矿业/",
     dividendPerYear: 2,
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.1,
       price: [
-        { value: 26.61, quantity: 500 },
-        { value: 25.61, quantity: 800 },
-        { value: 24.61, quantity: 1000 },
+        { value: 26.61, quantity: 200 },
+        { value: 25.61, quantity: 200 },
+        { value: 24.61, quantity: 200 },
       ],
     },
   },
@@ -378,7 +369,6 @@ const stocks: StockItem[] = [
     // 保利物业
     code: "06049",
     industry: Industry.PROPERTY_MANAGEMENT,
-    sharesHeld: 600,
     url: "/value-investing/industry/物业/保利物业/",
     dividendAdjust: 0.8,
     dividendPerYear: 1,
@@ -392,7 +382,7 @@ const stocks: StockItem[] = [
     // 赛轮轮胎
     code: "601058",
     industry: Industry.AUTOMOTIVE_AND_PARTS,
-    sharesHeld: 1000,
+    sharesHeld: 300,
     url: "/value-investing/industry/汽车/赛轮轮胎/",
     dividendPerYear: 2,
     remark: "",
@@ -400,8 +390,8 @@ const stocks: StockItem[] = [
       type: PlanType.PRICE,
       maxPositionRatio: 0.05,
       price: [
-        { value: 11.22, quantity: 800 },
-        { value: 10.62, quantity: 1000 },
+        { value: 11.22, quantity: 300 },
+        { value: 10.62, quantity: 400 },
       ],
     },
   },
@@ -409,7 +399,6 @@ const stocks: StockItem[] = [
     // 申洲国际
     code: "02313",
     industry: Industry.TEXTILES_AND_APPAREL,
-    sharesHeld: 200,
     url: "/value-investing/industry/纺织服装/申洲国际/",
     dividendPerYear: 2,
     dividendAdjust: 0.8,
@@ -477,21 +466,21 @@ const stocks: StockItem[] = [
     // 古井贡B
     code: "200596",
     industry: Industry.BAIJIU,
-    sharesHeld: 1000,
+    sharesHeld: 300,
     url: "/value-investing/industry/白酒/古井贡/",
     dividendPerYear: 2,
     remark: "预估6月中分红",
     plan: {
       type: PlanType.PRICE,
       maxPositionRatio: 0.05,
-      price: [{ value: 50, quantity: 1000 }],
+      price: [{ value: 50, quantity: 100 }],
     },
   },
   {
     // 宇通客车
     code: "600066",
     industry: Industry.AUTOMOTIVE_AND_PARTS,
-    sharesHeld: 400,
+    sharesHeld: 500,
     url: "/value-investing/industry/汽车/宇通客车/",
     dividendPerYear: 2,
     remark: "2025年99.65%分红率，26Q1营收同比下滑7.92%",
@@ -512,8 +501,8 @@ const stocks: StockItem[] = [
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.05,
       dividend: [
-        { value: 0.058, quantity: 300 },
-        { value: 0.062, quantity: 500 },
+        { value: 0.058, quantity: 100 },
+        { value: 0.062, quantity: 200 },
       ],
     },
   },
@@ -566,13 +555,13 @@ const stocks: StockItem[] = [
     // 中国平安
     code: "601318",
     industry: Industry.INSURANCE,
-    sharesHeld: 200,
+    sharesHeld: 100,
     url: "/value-investing/industry/保险/中国平安/",
     dividendPerYear: 2,
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.05,
-      dividend: [{ value: 0.058, quantity: 400 }],
+      dividend: [{ value: 0.058, quantity: 200 }],
     },
   },
   {
@@ -596,14 +585,14 @@ const stocks: StockItem[] = [
     // 小商品城
     code: "600415",
     industry: Industry.ENTERPRISE_SERVICES,
-    sharesHeld: 1800,
+    sharesHeld: 1100,
     url: "/value-investing/industry/企业服务/小商品城/",
     dividendPerYear: 1,
     remark: "25年有一次性收入，分红率提高，26年分红绝对值未必能维持",
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.05,
-      dividend: [{ value: 0.052, quantity: 1000 }],
+      dividend: [{ value: 0.052, quantity: 400 }],
     },
   },
   {
@@ -623,7 +612,6 @@ const stocks: StockItem[] = [
     // 中远海控A
     code: "601919",
     industry: Industry.SHIPPING,
-    sharesHeld: 200,
     url: "/value-investing/industry/航运/中远海控/",
     dividendPerYear: 2,
     plan: {
@@ -636,7 +624,6 @@ const stocks: StockItem[] = [
     // 中远海控H
     code: "01919",
     industry: Industry.SHIPPING,
-    sharesHeld: 1000,
     url: "/value-investing/industry/航运/中远海控/",
     dividendAdjust: 0.8,
     dividendPerYear: 2,
@@ -650,7 +637,6 @@ const stocks: StockItem[] = [
     // 比亚迪
     code: "002594",
     industry: Industry.AUTOMOTIVE_AND_PARTS,
-    sharesHeld: 100,
     url: "/value-investing/industry/汽车/比亚迪/",
     dividendAdjust: 0.8,
     dividendPerYear: 1,
