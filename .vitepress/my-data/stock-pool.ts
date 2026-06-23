8ae2947f94b118ab42dcb70e9f58f822ec1169ef
qiushiming <qiushiming1997@gmail.com>
2026-06-23 14:08:05 +0800

    docs: update plan

--- .vitepress/my-data/stock-pool.ts
+++ .vitepress/my-data/stock-pool.ts
@@ -192,11 +192,11 @@ const stocks: StockItem[] = [
     // sharesHeld: 2000,
     url: "/value-investing/industry/塑料包装/永新股份/",
     remark:
-      "2025年分红率有降低，分红比过去两年低；6.23涨停清仓2000股，5%股息率接回来",
+      "2025年分红率有降低，分红比过去两年低；6.23涨停清仓2000股，5.2%股息率接回来",
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.05,
-      dividend: [{ value: 0.05, quantity: 2000 }],
+      dividend: [{ value: 0.052, quantity: 2000 }],
       // price: [
       //   { value: 9.7, quantity: 1400 },
       //   { value: 9.2, quantity: 1600 },
@@ -370,9 +370,9 @@ const stocks: StockItem[] = [
       type: PlanType.PRICE,
       maxPositionRatio: 0.1,
       price: [
-        { value: 26.61, quantity: 500 },
-        { value: 25.61, quantity: 800 },
-        { value: 24.61, quantity: 1000 },
+        { value: 26.61, quantity: 400 },
+        { value: 25.61, quantity: 600 },
+        { value: 24.61, quantity: 800 },
       ],
     },
   },
@@ -462,10 +462,7 @@ const stocks: StockItem[] = [
     plan: {
       type: PlanType.DIVIDEND,
       maxPositionRatio: 0.05,
-      dividend: [
-        { value: 0.075, quantity: 100 },
-        { value: 0.08, quantity: 200 },
-      ],
+      dividend: [{ value: 0.08, quantity: 100 }],
     },
   },
   {
