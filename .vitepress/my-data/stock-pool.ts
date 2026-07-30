f8832c1364049384cf043aac7c19f87ac51837ec
chenguobao <chenguobao@ava.com.cn>
2026-07-30 15:32:37 +0800

    docs: update plan

--- .vitepress/my-data/stock-pool.ts
+++ .vitepress/my-data/stock-pool.ts
@@ -541,14 +541,17 @@ const stocks: StockItem[] = [
     code: "000651",
     industry: Industry.HOME_APPLIANCES,
     qualityScore: 3,
-    sharesHeld: 400,
+    sharesHeld: 300,
     url: "/value-investing/industry/家电/格力/",
     dividendPerYear: 2,
     remark: "预估8月分红",
     maxPositionRatio: 0.05,
     plan: {
       type: PlanType.PRICE,
-      price: [{ value: 36, quantity: 100 }],
+      price: [
+        { value: 41.18, quantity: 100 },
+        { value: 36, quantity: 100 },
+      ],
     },
     exit: {
       type: PlanType.DIVIDEND,
@@ -663,6 +666,7 @@ const stocks: StockItem[] = [
     plan: {
       type: PlanType.PRICE,
       price: [
+        { value: 28, quantity: 100 },
         { value: 27, quantity: 200 },
         { value: 26, quantity: 300 },
         { value: 25, quantity: 400 },
@@ -690,6 +694,7 @@ const stocks: StockItem[] = [
     plan: {
       type: PlanType.PRICE,
       price: [
+        { value: 14, quantity: 200 },
         { value: 13.5, quantity: 300 },
         { value: 12.75, quantity: 400 },
       ],
@@ -972,7 +977,7 @@ const stocks: StockItem[] = [
     plan: {
       type: PlanType.DIVIDEND,
       dividend: [
-        { value: 0.086, quantity: 100 },
+        { value: 0.085, quantity: 100 },
         { value: 0.1, quantity: 200 },
       ],
     },
@@ -1140,7 +1145,7 @@ const stocks: StockItem[] = [
     plan: {
       type: PlanType.DIVIDEND,
       dividend: [
-        { value: 0.048, quantity: 200 },
+        { value: 0.0455, quantity: 200 },
         { value: 0.055, quantity: 300 },
       ],
     },
@@ -1422,6 +1427,6 @@ const stocks: StockItem[] = [
 ];
 
 /** 剩余现金（人民币），在持仓组合中作为"现金"显示，参与总市值和比例计算 */
-export const cash = ref(38000);
+export const cash = ref(42000);
 
 export { stocks };
